20c947846b5a5267990fa28f1b2b83af9ac6fbcf
Mickyelloow <michael.campisi@outlook.com>
2020-11-09 17:23:53 -0500

    - Menu nav mobile fonctionnel, mais toujours en cours de création

--- js/nav.js
+++ js/nav.js
@@ -4,44 +4,71 @@ var barreBas = document.getElementById("barre-bas");
 var boutonsNav = document.getElementById("site-navigation").querySelectorAll("li.page_item");
 var boutonsNavText = document.getElementById("site-navigation").querySelectorAll("a");
 var boutonsNavArr = Array.from(boutonsNav);
+var boutonNavBurger = document.getElementById("primary-menu-burger");
+var boutonNavBurgerBouton = document.getElementById("primary-menu-burger-bouton");
 
 var idBoutonASuivre = 0;
 var idBoutonOriginal = parseInt(document.getElementById("nav").getAttribute("data-pageid"));
 var estEnHover = false;
-idBoutonASuivre = idBoutonOriginal;
-
-boutonsNavText[idBoutonASuivre].style.color = "#6A07D2";
-boutonsNavText[idBoutonASuivre].style.fontWeight = "bold";
+var burgerOuvert = false;
 
 var timeout;
 
-for(var i = 0; i < boutonsNavArr.length; i++){
+(function() {
+    idBoutonASuivre = idBoutonOriginal;
 
-    boutonsNavArr[i].addEventListener("mouseenter", function (){
-        estEnHover = true;
-        idBoutonASuivre = boutonsNavArr.indexOf(this);
-        DeplacerBarres(idBoutonASuivre, "");
-        clearTimeout(timeout);
-    });
+    boutonsNavText[idBoutonASuivre].style.color = "#6A07D2";
+    boutonsNavText[idBoutonASuivre].style.fontWeight = "bold";
 
-    boutonsNavArr[i].addEventListener("mouseleave", function (){
-        estEnHover = false;
-        timeout = setTimeout(() => { 
-            if(estEnHover == false)
-                DeplacerBarres(idBoutonOriginal, "cubic-bezier(.4,0,.45,1)");
-        }, 500);
-    });
+    for(var i = 0; i < boutonsNavArr.length; i++){
 
-    boutonsNavArr[i].addEventListener("mousedown", function(){
-        barreHaut.style.backgroundColor = "#FFFFFF";
-        barreBas.style.backgroundColor = "#FFFFFF";
-    });
+        boutonsNavArr[i].addEventListener("mouseenter", function (){
+            estEnHover = true;
+            idBoutonASuivre = boutonsNavArr.indexOf(this);
+            DeplacerBarres(idBoutonASuivre, "");
+            clearTimeout(timeout);
+        });
+    
+        boutonsNavArr[i].addEventListener("mouseleave", function (){
+            estEnHover = false;
+            timeout = setTimeout(() => { 
+                if(estEnHover == false)
+                    DeplacerBarres(idBoutonOriginal, "cubic-bezier(.4,0,.45,1)");
+            }, 500);
+        });
+    
+        boutonsNavArr[i].addEventListener("mousedown", function(){
+            barreHaut.style.backgroundColor = "#FFFFFF";
+            barreBas.style.backgroundColor = "#FFFFFF";
+        });
+    
+        boutonsNavArr[i].addEventListener("mouseup", function(){
+            barreHaut.style.backgroundColor = "#FF04A1";
+            barreBas.style.backgroundColor = "#DFC516";
+        });
+    }
+});
 
-    boutonsNavArr[i].addEventListener("mouseup", function(){
-        barreHaut.style.backgroundColor = "#FF04A1";
-        barreBas.style.backgroundColor = "#DFC516";
+// pour une raison quelconque, il faut ajouter les styles et les events sur les éléments de la nav version burger après un petit délai
+setTimeout(() => { 
+    boutonNavBurger.style.transform = "translate(100%, 0px)";
+    boutonNavBurger.style.transition = "transform 0.5s";
+
+    boutonNavBurgerBouton.addEventListener("mousedown", function(){
+        console.log("AAAAAAAAAAA");
+        console.log(boutonNavBurger.style.transform.toString());
+        if(burgerOuvert == false){
+            boutonNavBurger.style.transform = "translate(0%, 0px)";
+            boutonNavBurgerBouton.style.transform = "translate(-120%, 0) rotate(90deg)";
+            burgerOuvert = true;
+        }
+        else{
+            boutonNavBurger.style.transform = "translate(100%, 0px)";
+            boutonNavBurgerBouton.style.transform = "translate(-120%, 0) rotate(0)";
+            burgerOuvert = false;
+        }
     });
-}
+}, 100);
 
 setTimeout(() => { 
     DeplacerBarres(idBoutonOriginal, "");
@@ -54,9 +81,14 @@ window.onresize = (function(){
     }, 300);
 })
 
-function DeplacerBarres(idBouton, ease){
+function DeplacerBarres(idBouton, ease, initial){
     //console.log(this);
     //console.log("OOF");
+
+    if(initial == false){
+        barreHaut.style.transition = "margin-left 1.5s, width 1s, background-color 0.1s";
+        barreBas.style.transition = "margin-left 2.25s, width 1.5s, background-color 0.1s";
+    }
 
     if(ease != ""){
         barreHaut.style.transitionTimingFunction = ease;
