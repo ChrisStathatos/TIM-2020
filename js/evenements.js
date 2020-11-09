f03e6ddb4f7142b25821e907a911200b658293a1
Mathieu Gaudreau <e1825699@cmaisonneuve.qc.ca>
2020-11-09 15:57:28 -0500

    d/but popup evenement

---
+++ js/evenements.js
@@ -0,0 +1,42 @@
+var slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) 
+{
+  var i;
+  var slides = document.getElementsByClassName("imgSelect");
+  var dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+}
+
+function poppup()
+{
+document.getElementById("popup").style.display = "grid";
+document.getElementById("cover").style.display = "block";
+
+}
+
+function fermerPopUp()
+{
+        document.getElementById("popup").style.display = "none";
+        document.getElementById("cover").style.display = "none";
+}
