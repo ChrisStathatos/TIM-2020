3ef072e3e41fd8454903a676c34dd1aa0f49c3cb
Abdirahman <e1703548@cmaisonnuenve.qc.ca>
2020-11-28 22:27:19 -0500

    equipe et json

--- js/profs.vue
+++ js/profs.vue
@@ -1,10 +1,27 @@
 <template>
   <div>
       <section v-for="(prof,index) in lesProfs" :key="index">
-          <!-- <img :src="{{prof.photo}}" alt=""> -->
-          <h1>{{prof.nom}}</h1>
-          <h2></h2>
-          <vue-markdown></vue-markdown>
+   <div :id="'#' + index + 'profs'" v-for="(profs,index) in lesProfs" :key="index" >
+
+       <!-- <div :id="prof.nom" class="section-profs">
+          <div class="grid-equipe">
+             <div class="boite-photo prof1">
+                <a class="btn" onclick="poppup()" :href="'#' + profs.nom">{{profs.nom}} </a>
+
+             </div>
+
+          </div>
+    </div> -->
+
+   <div id="popup">
+            <div class="boitePortraitPopup" ><img class="imgProfPopup" src=""  alt=""></div>
+            <h3 id="nomProf" >{{profs.nom}}</h3>
+            <h4 id="courrielProf">{{profs.courriel}}</h4>
+            <p id="description">{{profs.description}}</p>
+            <h5 id="coursDonnes">Cours donnés :...</h5>
+        </div>
+
+   </div>
       </section>
   </div>
 </template>
