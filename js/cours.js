70a3a4f0872622ea2f34b5e8977f005a7b936269
Christopher Stathatos <christopherstathatos@hotmail.com>
2020-12-13 21:06:07 -0500

    avancement page cours

--- js/cours.js
+++ js/cours.js
@@ -37,8 +37,8 @@ fetch("data/cours.json")
                                 <a class="btn" href="#${unCours[0].nom}"><p class="textBtn">${unCours[0].nomCourt}</p></a>
                                 <a class="btn cache" href="#${unCours[1].nom}"><p class="textBtn">${unCours[1].nomCourt}</p></a>
                                 <div class="dots">
-                                <span class="btnChoix active" data-cours="0" "onclick="changerCours(${i},${nombreCours})"></span>
-                                <span class="btnChoix" data-cours="1" "onclick="changerCours(${i},${nombreCours})"></span> 
+                                <div class="btnChoix active" onclick="changerCours(${i},${nombreCours},${1})"></div>
+                                <div class="btnChoix" onclick="changerCours(${i},${nombreCours},${2})"></div> 
                                 </div>
                             </div>
                         <div id="${unCours[0].nom}" class="modal-window" id="cover">
@@ -48,7 +48,7 @@ fetch("data/cours.json")
                         <div>
                         <h1 class="titre">${unCours[0].nom}<br><p class="ponderation">${unCours[0].ponderation}</p></h1>
                         </div>
-                        <p class="description" >${unCours[0].description}</p>
+                        <p class="description" >${unCours[0].description}</p> 
                         <img src="${unCours[0].photo}" class="imgCours">
                         <div class="prerequis"> <p>Cours prérequis:</p> <br> <p>${unCours[0].prealabe}</p></div>
                         <div class="heures"> <p>Nombres d'heures:</p> <br> <p>${unCours[0].nombreHeure}</p></div>
@@ -141,8 +141,28 @@ fetch("data/cours.json")
    }
 }
 
-function changerCours(session,cours){
+function changerCours(session,cours,bouton){
+  session-=1;
+  selectionCours = document.getElementsByClassName("sessions")[session].lastElementChild.children[cours];
+  if(bouton == 1){
+    selectionCours.firstElementChild.lastElementChild.firstElementChild.classList.add("active");
+    selectionCours.firstElementChild.lastElementChild.lastElementChild.classList.remove("active");
+    selectionCours.firstElementChild.children[0].classList.remove("cache");
+    selectionCours.firstElementChild.children[1].classList.add("cache");
+    selectionCours.children[1].classList.remove("cache");
+    selectionCours.children[2].classList.add("cache");
 
+
+  }
+  else if(bouton == 2){
+    selectionCours.firstElementChild.lastElementChild.firstElementChild.classList.remove("active");
+    selectionCours.firstElementChild.lastElementChild.lastElementChild.classList.add("active");
+    selectionCours.firstElementChild.children[0].classList.add("cache");
+    selectionCours.firstElementChild.children[1].classList.remove("cache");
+    selectionCours.children[1].classList.add("cache");
+    selectionCours.children[2].classList.remove("cache");
+    
+  }
 
 }
 
